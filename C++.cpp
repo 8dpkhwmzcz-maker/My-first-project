0e4429936be862821d4be23c950c4edc58f561c9
8dpkhwmzcz-maker <8dpkhwmzcz@privaterelay.appleid.com>
2025-11-14 12:11:22 +0300

    Add files via upload

---
+++ C++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  double B, A;
+  cout << "Enter the number "<< endl;
+  cin >> B ;
+  A=B*3.14*B;
+  cout << "The Area of circle is: " << A << endl;
+  return 0;
+}
