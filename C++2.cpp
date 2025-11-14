3543cf0251bda7627ef5b52ac629135895c4554b
8dpkhwmzcz-maker <8dpkhwmzcz@privaterelay.appleid.com>
2025-11-14 14:32:15 +0300

    Add files via upload

---
+++ C++2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  double c,f;
+
+ cout << "Enter your name: ";
+
+ cin >> c;
+
+ f = 5.0/9.0 * (c - 32);
+ cout << "The fahrenheit is: " << f << endl;
+
+
+}
