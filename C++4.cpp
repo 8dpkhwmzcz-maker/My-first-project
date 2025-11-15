b88ada9c2eb68e34748a6bfd0369741469044bff
8dpkhwmzcz-maker <8dpkhwmzcz@privaterelay.appleid.com>
2025-11-15 20:25:13 +0300

    Add files via upload

---
+++ C++4.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    cout << "The square root of 16 is: " << sqrt(16) << endl;
+    cout << "The pwoer of 2 raised to 3 is: " << pow(2 , 3) << endl;
+    cout << "The floor value of 5.7 is: " << floor(5.7) << endl;
+    cout << "The ceiling value of 7.2 is: " << ceil(7.2) << endl;
+    cout << "The absolute value of -10 is: " << abs(-10) << endl;
+    
+}
+
